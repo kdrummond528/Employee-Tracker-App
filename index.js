1bcc4324e81fbcb64884f4002d017802b23aa44c
Karina Drummond <k.drummond528@gmail.com>
2022-11-04 09:54:51 -0700

    modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const { default: ListPrompt } = require("inquirer/lib/prompts/list");
 
 // connection to employee db
 const database = mysql.createConnection({
@@ -12,11 +13,27 @@ const database = mysql.createConnection({
 
 database.connect((err) => {
     if (err) throw err;
-    beginQuestions();
+    beginPrompt();
 });
 
-function beginQuestions() {
+// funtion to begin inquirer prompt
+function beginPrompt() {
     inquirer.prompt({
-
+        name: "options",
+        type: "list",
+        message: "Please make a selection.",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Exit"]
     })
+        .then((answer) => {
+            if 
+
+    });
 }
