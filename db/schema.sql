fd2d0b1b682ad1e30c46315ff9288182ade5b71c
Karina Drummond <k.drummond528@gmail.com>
2022-11-04 10:22:30 -0700

    modified schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,27 @@
+-- drops the employees database if it already exists --
 DROP DATABASE IF EXISTS employees_db;
+-- creates the employees database --
 CREATE DATABASE employees_db;
 
+-- uses the employees database --
 USE employees_db;
 
-CREATE TABLE departments();
+-- creates the tables for department, role, and employee database --
+CREATE TABLE departments(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30)
+);
 
-CREATE TABLE roles();
+CREATE TABLE roles(
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT
+);
 
-CREATE TABLE employees();
+CREATE TABLE employees(
+
+);
+
+-- see the employees database in use --
+SELECT DATABASE();
