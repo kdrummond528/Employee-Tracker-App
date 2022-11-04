23619f45f072c7c9cf2eb722f2614361b0f160ad
Karina Drummond <k.drummond528@gmail.com>
2022-11-04 10:37:44 -0700

    modified schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,23 +8,29 @@ USE employees_db;
 
 -- creates the tables for department, role, and employee database --
 CREATE TABLE departments(
-id INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 name VARCHAR(30)
 );
 
 CREATE TABLE roles(
-id INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 title VARCHAR(30),
 salary DECIMAL,
-department_id INT
+department_id INT,
+FOREIGN KEY (department_id) 
+REFERENCES department(id)
 );
 
 CREATE TABLE employees(
-id INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
-manager_id INT
+FOREIGN KEY (role_id)
+REFERENCES role(id),
+manager_id INT,
+ FOREIGN KEY (manager_id)
+REFERENCES employee(id)
 );
 
 -- see the employees database in use --
