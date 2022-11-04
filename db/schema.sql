fedd1b6244135731e43a2bbe20062f9759db58a7
Karina Drummond <k.drummond528@gmail.com>
2022-11-04 10:22:58 -0700

    modified schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -20,7 +20,11 @@ department_id INT
 );
 
 CREATE TABLE employees(
-
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT
 );
 
 -- see the employees database in use --
